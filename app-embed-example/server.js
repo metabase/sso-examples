597f868e268837cc3dceec283ad0fd36d261ce89
Tom Robinson <tlrobinson@gmail.com>
2019-09-23 12:57:48 -0700

    Add metalbase_user_id to app embed user JWT

--- app-embed-example/server.js
+++ app-embed-example/server.js
@@ -10,15 +10,24 @@ const METABASE_JWT_SHARED_SECRET =
   process.env.METABASE_JWT_SHARED_SECRET ||
   "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
 
+// This matches a user in the Sample Dataset
+const DEMO_USER = {
+  id: 8,
+  email: "aracely.jenkins@gmail.com",
+  first_name: "Aracely",
+  last_name: "Jenkins"
+};
+
 const signUserToken = user =>
   jwt.sign(
     {
       email: user.email,
       first_name: user.first_name,
       last_name: user.last_name,
-      exp: Math.round(Date.now() / 1000) + (60 * 10) // 10 minute expiration
+      metalbase_user_id: user.id,
+      exp: Math.round(Date.now() / 1000) + 60 * 10 // 10 minute expiration
     },
-    METABASE_JWT_SHARED_SECRET,
+    METABASE_JWT_SHARED_SECRET
   );
 
 const app = express();
@@ -30,19 +39,15 @@ app.get("/api/config", (req, res) => res.json({ METABASE_URL }));
 app.get("/api/auth/metabase", (req, res) => {
   // NOTE: in a real application you would use the embedding application's session to determine which user to use here.
   // If no session exists you would need to show an error or redirect to the embedding application's login page.
-  const user = {
-    email: "embedtest@metabase.com",
-    first_name: "Hello",
-    last_name: "World",
-  };
+  const user = DEMO_USER;
   res.redirect(
     url.format({
       pathname: `${METABASE_URL}/auth/sso`,
       query: {
         jwt: signUserToken(user),
-        return_to: req.query.return_to,
-      },
-    }),
+        return_to: req.query.return_to
+      }
+    })
   );
 });
 
